83e4aec71f614da251fab1497fc7d4ca3cee15b2
seungyoon <cho31250@gmail.com>
2021-04-07 00:41:02 +0900

    eureka setting

--- src/main/java/spring/cloud/eureka/EurekaApplication.java
+++ src/main/java/spring/cloud/eureka/EurekaApplication.java
@@ -2,8 +2,11 @@ package spring.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
